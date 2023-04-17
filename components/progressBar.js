e20092e01ba6122cfb75ee19259d82f1f7970eb1
Lu Wang <luwang.wang@gmail.com>
2023-04-17 14:03:46 -0400

    added multiple progress bars

--- components/progressBar.js
+++ components/progressBar.js
@@ -1,12 +1,8 @@
 class ProgressBar extends HTMLElement {
     connectedCallback() {
-        // var donePct = Number(this.attributes.donePct.value);
-        // var inProgressPct = Number(this.attributes.inProgressPct.value);
-        // var notStartedPct = Number(this.attributes.notStartedPct.value);
-
-        var donePct = 50;
-        var inProgressPct = 25;
-        var notStartedPct = 25; // 100 - donePct - inProgressPct
+        var donePct = Number(this.attributes.donePct.value);
+        var inProgressPct = Number(this.attributes.inProgressPct.value);
+        var notStartedPct = 100 - donePct - inProgressPct;
 
         this.innerHTML =    `<div class="progress-bar columns">
                                 <div class="done-segment progress-segment" style="width: ${donePct}%;">
