765f021b8952e12c0a09b78e02ae835eeb05d683
Lu Wang <luwang.wang@gmail.com>
2023-04-17 15:05:57 -0400

    relocated the progressBar behavior code

--- components/progressBar.js
+++ components/progressBar.js
@@ -1,3 +1,4 @@
+/** Custom HTML progress-bar element. */
 class ProgressBar extends HTMLElement {
     connectedCallback() {
         var donePct = Number(this.attributes.donePct.value);
@@ -14,5 +15,57 @@ class ProgressBar extends HTMLElement {
                             </div>`;
     }    
 }
-
 customElements.define('progress-bar', ProgressBar);
+
+
+/** Toggles the right-side panel.
+         * First resets all background and coloring inversion, then styles accordingly. */
+function toggleRightPanel(callingProgressBarHero) {
+    var rhs = document.getElementById("main-right");
+    var progressBar = callingProgressBarHero.querySelector("progress-bar").children[0];
+    
+    uninvertAll();
+    if (rhs.style.display === "none") {
+        showRightPanel();
+        callingProgressBarHero.classList.add("has-background-dark");
+        invertColors(progressBar);
+    } else if (rhs.style.display === "block") {
+        hideRightPanel();
+    }
+}
+
+    /** Show right-side panel. */
+    function showRightPanel() {
+        document.getElementById("main-right").style.display = "block";
+    }
+
+    /** Hide right-side panel. */
+    function hideRightPanel() {
+        document.getElementById("main-right").style.display = "none";
+    }
+
+    /** Invert progress bar segment and border colors. */
+    function invertColors(progressBar) {
+        var progressBarSegments = progressBar.children;
+        progressBarSegments[0].style.backgroundColor = "white";
+        progressBarSegments[2].style.backgroundColor = "#5178FF";
+        progressBar.style.borderColor = "white";
+    }
+
+    /** Un-invert progress bar segment and border colors. */
+    function uninvertColors(progressBar) {
+        var progressBarSegments = progressBar.children;
+        progressBarSegments[2].style.backgroundColor = "white";
+        progressBarSegments[0].style.backgroundColor = "#5178FF";
+        progressBar.style.borderColor = "#5178FF";
+    }
+
+    /** Un-inverts all progress bar's segment and border coloring. */
+    function uninvertAll() {
+        const allProgressBars = document.getElementsByClassName("progress-bar-container");
+        for (let i = 0; i < allProgressBars.length; i++) {
+            var progressBar = allProgressBars[i].querySelector("progress-bar").children[0];
+            uninvertColors(progressBar);
+            allProgressBars[i].classList.remove("has-background-dark");
+        }
+    }
