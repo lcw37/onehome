7d64ec363bbca7753fd9e3ce6df8bbd2cfb930c6
Lu Wang <luwang.wang@gmail.com>
2023-04-17 10:52:25 -0400

    added coloring and value-based width

--- components/progressBar.js
+++ components/progressBar.js
@@ -4,12 +4,20 @@ class ProgressBar extends HTMLElement {
         // var inProgressPct = Number(this.attributes.inProgressPct.value);
         // var notStartedPct = Number(this.attributes.notStartedPct.value);
 
-        var donePct = 0.5;
-        var inProgressPct = 0.25;
-        var notStartedPct = 0.25; // 1 - donePct - inProgressPct
+        var donePct = 50;
+        var inProgressPct = 25;
+        var notStartedPct = 25; // 100 - donePct - inProgressPct
 
-        this.innerHTML =    `<div class="progress-bar">
-                                ${donePct}<br>${inProgressPct}<br>${notStartedPct}
+        this.innerHTML =    `<div class="progress-bar columns">
+                                <div class="progress-segment" style="background-color: #5178FF; width: ${donePct}%">
+                                    ${donePct}
+                                </div>
+                                <div class="progress-segment" style="background-color: #5178FF; opacity: 50%; width: ${inProgressPct}%">
+                                    ${inProgressPct}
+                                </div>
+                                <div class="progress-segment" style="background-color: white; width: ${notStartedPct}%">
+                                    ${notStartedPct}
+                                </div>
                             </div>`;
         // this.getElementsByClassName("done-segment").style.width = "10px";
         // this.getElementsByClassName("inProgress-segment").style.width = "15px";
