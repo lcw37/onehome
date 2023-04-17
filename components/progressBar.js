881fea30425f02b086cc6f80aeec48e34745caf3
Lu Wang <luwang.wang@gmail.com>
2023-04-16 20:50:53 -0400

    saving changes: working on progress bar

--- components/progressBar.js
+++ components/progressBar.js
@@ -1,13 +1,19 @@
 class ProgressBar extends HTMLElement {
     connectedCallback() {
-        var donePct = Number(this.attributes.donePct.value);
-        var inProgressPct = Number(this.attributes.inProgressPct.value);
-        var notStartedPct = Number(this.attributes.notStartedPct.value);
+        // var donePct = Number(this.attributes.donePct.value);
+        // var inProgressPct = Number(this.attributes.inProgressPct.value);
+        // var notStartedPct = Number(this.attributes.notStartedPct.value);
+
+        var donePct = 0.5;
+        var inProgressPct = 0.25;
+        var notStartedPct = 0.25; // 1 - donePct - inProgressPct
 
         this.innerHTML =    `<div class="progress-bar">
-                                
                                 ${donePct}<br>${inProgressPct}<br>${notStartedPct}
                             </div>`;
+        // this.getElementsByClassName("done-segment").style.width = "10px";
+        // this.getElementsByClassName("inProgress-segment").style.width = "15px";
+        // this.getElementsByClassName("notStarted-segment").style.width = "20px";
     }
 }
 
